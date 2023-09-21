55d6bd466e086803cc503a30b005cd56b0414a34
Marketing Director <nicolestark1103@gmail.com>
2023-09-21 08:37:39 -0500

    recipe book route and add recipe routes working

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -22,6 +22,30 @@ router.get('/', withAuth, async (req, res) => {
     }
 })
 
+router.get('/recipes/:id', async (req, res) => {
+    try {
+        const recipe = await Recipe.findOne({
+            where: {id: req.params.id},
+            include: [
+                User,
+                {
+                    model: Comment,
+                    include: [User]
+                }
+            ]
+        })
+
+        let singleRecipe = recipe.get({ plain: true })
+        res.render('recipe', {
+            singleRecipe,
+            logged_in: req.session.logged_in
+        })
+    } catch(err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+})
+
 router.get('/login', (req, res) => {
     if (req.session.logged_in) {
       res.redirect('/');
@@ -42,6 +66,48 @@ router.get('/signup', (req, res) => {
       res.render('signup')
     } catch(err) {
       res.status(500).json(err)
+    }
+  })
+
+  router.get('/recipe-book', async (req, res) => {
+    const user_id = req.session.user_id
+    
+    if(!user_id) {
+        res.redirect('/login')
+    }
+
+    try {
+        const user = await User.findByPk(user_id, {
+            raw: true
+        })
+
+        const recipes = await Recipe.findAll({
+            where: {
+                user_id
+            },
+            raw: true
+        })
+
+        const comments = await Comment.findAll({
+            where: {
+                user_id
+            },
+            raw: true
+        })
+
+        res.render('recipe-book', {...user, recipes, comments, logged_in: req.session.logged_in})
+
+    } catch(err) {
+        console.log(err)
+        res.status(500).json(err)
+    }
+  })
+
+  router.get('/add-recipe', (req, res) => {
+    try {
+        res.render('add-recipe', {logged_in: req.session.logged_in})
+    } catch(err) {
+        res.status(500).json(err)
     }
   })
 
